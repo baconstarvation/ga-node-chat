14137afad24c885c9375ca08c1f005078e1c6dea
baconstarvation <ant.ucd@gmail.com>
2013-12-04 20:29:45 -0800

    ch ch ch ch ch changes

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
+/*    
+Server
+Need to send html, css, javascript
+establish connnections with clients
+listen to clients for new users
+listen for messages from users
+listen for disconnects
+
+client
+establish a connection with the server
+listen for new messages from others
+listen for updates about who is in the chatroom
+listen for messages from the user to send to server
+update the DOM appropriately
+
+*/
+
 var app = require('http').createServer(handler);
-var io = require('socket.io').listen(app);
+var io = require('socket.io').listen(app); // socket.io is a module from npm
 var fs = require('fs');
  
 app.listen(8080);
  
 function handler (req, res) {
-        fs.readFile(__dirname + '/index.html', function(err, data) {
-                if (err) {
-                        res.writeHead(500);
-                        return res.end('Error loading index.html');
+	fs.readFile(__dirname + '/index.html', function(err, data) { //__dirname is name of current directory. in the directory that we're in, we want you to serve the file index.html
+		if (err) {
+			res.writeHead(500);
+			return res.end('Error loading index.html');
                 }
  
                 res.writeHead(200);
@@ -18,9 +35,9 @@ function handler (req, res) {
  
 var users = {};
  
-io.sockets.on('connection', function(socket) {
+io.sockets.on('connection', function(socket) { // establish a connection
         var username;
-        socket.on('newUser', function(user) {
+        socket.on('newUser', function(user) { // listen for new users
  
                 username = user;
  
@@ -31,11 +48,11 @@ io.sockets.on('connection', function(socket) {
                 io.sockets.emit('updateUsers', users);
         });
  
-        socket.on('message', function(message) {
+        socket.on('message', function(message) { // listen for msgs from the client
                 io.sockets.emit('newMessage', username, message);
         });
  
-        socket.on('disconnect', function() {
+        socket.on('disconnect', function() { // listen for when a user leaves the cat room
                 delete users[username];
  
                 io.sockets.emit('updateUsers', users);
